ff71c7a010540323b0e3ca2acb7589cfe7f86ae0
LaPluses <xipersup@gmail.com>
2019-03-30 00:20:42 +0800

    [Preview]: Update upload processing logic.

--- src/components/Preview/App.tsx
+++ src/components/Preview/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { type } from "os";
 
 // MimeType, ref to https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-enum AllowedMimeType {
+enum VideoMineType {
   AVI = "video/x-msvideo",
   MPEG = "video/mpeg",
   OGG = "video/ogg",
-  WEBM = "video/webm",
+  WEBM = "video/webm"
+}
+
+enum ImageMineType {
   JPG = "image/jpeg",
   GIF = "image/gif",
   PNG = "image/png"
@@ -17,21 +21,34 @@ interface Props {
 
 export default class extends React.Component<Props, any> {
   render() {
-    if (!this.props.selectedFile) {
-      return <div> Please upload a photo </div>;
+    const file = this.props.selectedFile;
+    if (!file) {
+      return <div> Please upload image or video </div>;
     }
-    const type = this.props.selectedFile.type;
-    let allowed = false;
-    for (let each in AllowedMimeType) {
-      if (type === AllowedMimeType[each]) {
-        allowed = true;
+    const type = file.type;
+    let isVideo = false;
+    let isImage = false;
+    for (let each in VideoMineType) {
+      if (type === VideoMineType[each]) {
+        isVideo = true;
         break;
       }
     }
-    if (!allowed) {
-      return <div> Please choose right picture | video. </div>;
+    for (let each in ImageMineType) {
+      if (type === ImageMineType[each]) {
+        isImage = true;
+        break;
+      }
+    }
+    if (!isVideo && !isImage) {
+      return <div> Please choose right image or video </div>;
     } else {
-      return <div> Good job </div>;
+      const url = window.URL.createObjectURL(file);
+      if (isVideo) {
+        return <video src={url} />;
+      } else {
+        return <img src={url} />;
+      }
     }
   }
 }
